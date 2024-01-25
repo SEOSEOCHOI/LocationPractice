5b70a519dd834cfb77e89012eafb434fa6cfe054
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-01-25 21:56:42 +0900

    22회차 과제

--- LocationPractice/Theater/TheaterViewController.swift
+++ LocationPractice/Theater/TheaterViewController.swift
@@ -8,19 +8,31 @@
 import UIKit
 import MapKit
 
-enum TheaterType: String, CaseIterable {
+enum TheaterType: Int, CaseIterable {
     case 롯데시네마
     case 메가박스
     case CGV
+    case total
+    
+    var title: String {
+        switch self {
+        case .롯데시네마:
+            return "롯데시네마"
+        case .메가박스:
+            return "메가박스"
+        case .CGV:
+            return "CGV"
+        case .total:
+            return "전체보기"
+        }
+    }
 }
 
 
 class TheaterViewController: UIViewController {
-    
-
-    
     let theaterAnnotation: [Theater] = TheaterList.mapAnnotations
     var annotationList: [MKPointAnnotation] = []
+    let theaterNameList = TheaterType.allCases
     
     @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
@@ -41,126 +53,45 @@ class TheaterViewController: UIViewController {
             annotation.title = theater.location
             
             annotationList.append(annotation)
-            
         }
-        
-        for annotationData in annotationList {
-            mapView.addAnnotation(annotationData)
-        }
+        mapView.addAnnotations(annotationList)
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterButtonClicked))
     }
     
-    func showAlert(title: String, message: String, buttonTitle: [String], completionHandler: @escaping () -> Void) {
+    func showAlert(completionHandler: @escaping (String) -> Void) {
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
         
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-        
-        for count in 0 ... buttonTitle.count - 1 {
-            let action = UIAlertAction(title: buttonTitle[count], style: .default) { _ in
+        for type in theaterNameList {
+            let action = UIAlertAction(title: type.title, style: .default) { _ in
                 
-                completionHandler()
+                completionHandler(type.title)
             }
             alert.addAction(action)
         }
-        
         present(alert, animated: true)
     }
     
-    func isContain(type: String, A: [Theater]) -> [MKAnnotation] {
-        var annotationData: [MKAnnotation] = []
-        for theater in A {
-            if theater.type == type {
-                let coordinate =  CLLocationCoordinate2D(latitude: theater.latitude, longitude: theater.longitude)
-                
-                let annotation = MKPointAnnotation()
-                
-                annotation.coordinate = coordinate
-                annotation.title = theater.location
-                
-                annotationData.append(annotation)
-            }
-        }
-        return annotationData
-    }
-    
     @objc func filterButtonClicked() {
-        var buttonTitle: [String] = []
-        for type in TheaterType.allCases {
-            buttonTitle.append(type.rawValue)
-        }
-        buttonTitle.append("전체보기")
-        
-        showAlert(title: "", message: "", buttonTitle: buttonTitle) {
-
-                        self.mapView.removeAnnotations(self.mapView.annotations)
-
-            isContain(type: action.title, A: theaterAnnotation)
-            }
+        showAlert() { action in
+            print(action)
+            var annotationData: [MKAnnotation] = []
+            self.mapView.removeAnnotations(self.mapView.annotations)
             
-            //            self.mapView.addAnnotations(annotationData)
-
-    
+            if action == TheaterType.total.title {
+                annotationData = self.annotationList
+                self.mapView.addAnnotations(annotationData)
+            } else {
+                for annotation in self.annotationList {
+                    guard let theater = annotation.title else { return }
+                    if theater.contains(action) {
+                        annotationData.append(annotation)
+                    }
+                    self.mapView.addAnnotations(annotationData)
+                    
+                }
+            }
         }
-
-//
-//        let lotteCinema = UIAlertAction(title: "LotteCinema", style: .default) { _ in
-//            self.mapView.removeAnnotations(self.mapView.annotations)
-//            for annotation in self.annotationList {
-//                if let title = annotation.title {
-//                    if title.contains(TheaterType.롯데시네마.rawValue) {
-//                        annotationData.append(annotation)
-//                    }
-//                }
-//            }
-//            self.mapView.addAnnotations(annotationData)
-//
-//        }
-//        showAlert(title: "", message: "", buttonTitle: "MegaBox") {
-//            self.mapView.removeAnnotations(self.mapView.annotations)
-//
-//          
-//            for annotation in self.annotationList {
-//                if let title = annotation.title {
-//                    if title.contains(TheaterType.메가박스.rawValue) {
-//                        annotationData.append(annotation)
-//                    }
-//                }
-//            }
-//            self.mapView.addAnnotations(annotationData)
-//        }
-//        
-//        showAlert(title: "", message: "", buttonTitle: "cgv") {
-//            self.mapView.removeAnnotations(self.mapView.annotations)
-//
-//          
-//            for annotation in self.annotationList {
-//                if let title = annotation.title {
-//                    if title.contains(TheaterType.CGV.rawValue) {
-//                        annotationData.append(annotation)
-//                    }
-//                }
-//            }
-//            self.mapView.addAnnotations(annotationData)
-//        }
-
-
-
-//        
-//        
-//        
-//        let all = UIAlertAction(title: "전체보기", style: .default) { _ in
-//            self.mapView.addAnnotations(self.annotationList)
-//            print("전", annotationData)
-//
-//        }
-//        
-//
-//        actionSheet.addAction(lotteCinema)
-//        actionSheet.addAction(all)
-//        
-//        present(actionSheet, animated: true)
-//        
+        
     }
 }
-
-
